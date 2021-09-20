a3a1c4769edd62970c4e5dd2732f115ba715d603
Alena Bukilic <alena.bukilic@florencehc.com>
2021-09-20 12:22:28 +0200

    fix syntax

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-exports.upload = async (req, res) => {
+exports.upload = (req, res) => {
 
     const data = {
         name: req.payload.fileUp.filename,
         type: req.payload.fileUp.headers["content-type"],
         size: req.payload.fileUp.bytes
     }
-    return await data;
+    return data;
 }
